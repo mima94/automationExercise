b473cbbcd5da4f492c7d41809607f27b63a59af3
mima94 <milanadavidovac94@gmail.com>
2025-07-29 12:22:04 +0200

    new tests

--- tests/test-data/automation.exercise.api.spec.ts
+++ tests/test-data/automation.exercise.api.spec.ts
@@ -1,10 +1,25 @@
 import { test, expect, request } from '@playwright/test';
 
-
 test('GET /api/productsList - should return list of all products', async ({ request }) => {
-  const response = await request.get('/api/productsList');
-  const responseBody = await response.json();
-  expect(response.status()).toBe(200);
-  expect(responseBody).toHaveProperty('products');
-  expect(Array.isArray(responseBody.products)).toBe(true);
+    const response = await request.get('/api/productsList');
+    const responseBody = await response.json();
+    expect(response.status()).toBe(200);
+    expect(responseBody).toHaveProperty('products');
+    expect(Array.isArray(responseBody.products)).toBe(true);
+});
+
+test('POST /api/productsList - should return that method is not supported', async ({ request }) => {
+    const response = await request.post('/api/productsList');
+    const text = await response.text();
+    expect(response.status()).toBe(200);
+    expect(text).toContain('This request method is not supported.');
+});
+
+test('GET /api/brandList - should return all brands list', async ({ request }) => {
+    const response = await request.get('/api/brandList');
+    const data = await response.json();
+    expect(response.status()).toBe(200);
+    expect(data).toHaveProperty('brands');
+    expect(Array.isArray(data.brands)).toBe(true);
+
 });
