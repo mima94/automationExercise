8bf19143e23bde4046f8699b4c940ce823f21483
mima94 <milanadavidovac94@gmail.com>
2025-08-04 13:58:33 +0200

    put to all brands list

--- tests/test-data/automation.exercise.api.spec.ts
+++ tests/test-data/automation.exercise.api.spec.ts
@@ -15,11 +15,18 @@ test('POST /api/productsList - should return that method is not supported', asyn
     expect(text).toContain('This request method is not supported.');
 });
 
-test('GET /api/brandList - should return all brands list', async ({ request }) => {
+test('GET /api/brandsList - should return all brands list', async ({ request }) => {
     const response = await request.get('/api/brandsList');
     const data = await response.json();
     expect(response.status()).toBe(200);
     expect(data).toHaveProperty('brands');
     expect(Array.isArray(data.brands)).toBe(true);
-
 });
+
+test('PUT /api/brandsList - should return that request method is not supported', async ({ request }) => {
+    const response = await request.put('/api/brandsList');
+    const text = await response.text();
+    expect(response.status()).toBe(200);
+    expect(text).toContain('This request method is not supported.');
+});
+
